b430c6cc9127bc441cc829ed139a521fe17f67ca
dain Hong <ribbon0522@naver.com>
2020-05-31 18:31:43 +0900

    question

--- decisionProject/question/urls.py
+++ decisionProject/question/urls.py
@@ -6,12 +6,13 @@ import question.views
 
 urlpatterns = [
     path('', views.list, name="list"),
-    path('question/new', views.new, name="new"),
-    path('question/create', question.views.create, name="create"),
-    path('question/<int:question_id>',question.views.detail, name="detail"),
-    path('question/edit/<int:question_id>', question.views.edit, name="edit"),
-    path('question/update/<int:question_id>', question.views.update, name="update"),
-    path('question/delete/<int:question_id>',question.views.delete,name = "delete"),
-    # path('question/list', question.views.list, name = "list"),
-    #path('question/answer', views.answer, name="answer")
+    path('new/', views.new, name="new"),
+    path('create/', question.views.create, name="create"),
+    path('<int:question_id>',question.views.detail, name="detail"),
+    path('edit/<int:question_id>', question.views.edit, name="edit"),
+    path('update/<int:question_id>', question.views.update, name="update"),
+    path('delete/<int:question_id>',question.views.delete,name = "delete"),
+    # path('list', question.views.list, name = "list"),
+    path('create_answer/<int:question_id>', views.create_answer,name="create_answer"),
+    path('answer_edit/<int:question_id>',views.answer_edit,name="answer_edit"),
 ]
--- decisionProject/question/views.py
+++ decisionProject/question/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Answer
 from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 # Create your views here.
@@ -14,34 +14,68 @@ def new(request):
 def create(request):
     new_question = Question()
     new_question.title = request.POST['title']
+    new_question.writer = request.POST['writer']
     new_question.content = request.POST['content']
     new_question.pub_date = timezone.datetime.now()
     new_question.save()
-    return redirect('/question/question/'+ str(new_question.id))
+    return redirect('/question/'+ str(new_question.id))
 
 def detail(request,question_id):
     question = get_object_or_404(Question,pk = question_id)
-    return render(request, 'detail.html',{'question':question})
 
-def edit(request,question_id):
+    answer = Answer.objects.filter(question_id=question_id)
+    print(answer)
+    return render(request, 'detail.html',{'question':question, 'answer':answer})
+
+def edit(request,question_id):    
     question = get_object_or_404(Question,pk = question_id)
     return render(request,'edit.html',{'question':question})
 
 def update(request,question_id):
     edit_question = get_object_or_404(Question,pk = question_id)
     edit_question.title = request.POST['title']
+    edit_question.writer = request.POST['writer']
     edit_question.content = request.POST['content']
     edit_question.pub_date = timezone.datetime.now()
     edit_question.save()
-    return redirect('/question/question/'+ str(edit_question.id))
+    return redirect('/question/'+ str(edit_question.id))
 
 def delete(request,question_id):
     delete_question = get_object_or_404(Question,pk = question_id)
     delete_question.delete()
     return redirect ('list')
 
-def answer(request):
-    answer = Answer.objects.all()
-    return render(request, {'answer':answer})
 
-#answer 함수는 어떻게 해야할까..
+# def create_answer(request):
+#     return redirect('detail/'+ )
+
+
+def create_answer(request, question_id):
+    new_answer = Answer()
+    new_answer.writer = request.POST['writer']
+    new_answer.content = request.POST['content']
+    new_answer.pub_date = timezone.datetime.now()
+    new_answer.question_id = question_id
+    new_answer.save()
+
+    print('create test')
+    return redirect('detail',question_id) 
+    
+# detail_answer = Answer()
+#     detail_answer.writer = request.POST['writer']
+#     detail_answer.content = request.POST['content']
+#     detail_answer.pub_date = timezone.datetime.now()
+#     detail_answer.save()
+#     return redirect('detail')
+
+# def create(request, question_id):
+#     detail_answer = Answer()
+#     detail_answer.writer = request.POST['writer']
+#     detail_answer.content = request.POST['content']
+#     detail_answer.pub_date = timezone.datetime.now()
+#     detail_answer.save()
+#     return redirect('detail')
+def answer_edit(request,question_id):    
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request,'answer_edit.html',{'question':question})
+
