534badac52ebbc5954742952ecd314ae8b4d8b2a
labiss96 <cxz9080@gmail.com>
2020-06-03 16:52:17 +0900

    code edit.

--- decisionProject/main/urls.py
+++ decisionProject/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name="main"
+
 urlpatterns = [
     path('', views.home, name="home"),
+    path('mypage/<str:profile_name>', views.mypage, name="mypage"),
+    
 ]
