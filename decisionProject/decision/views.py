6f1125dc51f3b3d3f8f1baf3bc3fb8aee31e7e4f
labiss96 <cxz9080@gmail.com>
2020-06-03 19:28:44 +0900

    develop Decision service

--- decisionProject/decision/views.py
+++ decisionProject/decision/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Decision
-from .form import DecisionForm
 from django.shortcuts import render
 import random
 
@@ -8,24 +7,36 @@ import random
 # Create your views here.
 
 
-def list(request):
-    if request.method == 'POST':
-        form = DecisionForm(request.POST, request.FILES) #입력값
-        if form.is_valid():
-            content = form.save(commit = False) #임시저장
-            content.save() #저장
-            return render(request, 'result.html')
+def list(request):    
+    if request.user.is_authenticated:
+        # decisions = get_object_or_404(Decision, maker=request.user)
+        decisions = Decision.objects.filter(maker=request.user)
+        return render(request, 'decision/list.html', {'decisions': decisions})
     else:
-        form = DecisionForm()
-        return render(request, 'list.html', {'form':form})
+        return redirect('main:login')
 
 
 def result(request):
-    number = random.randrange(0,2)
+    if request.method=='POST':
+        content = request.POST['content']
+        number = random.randrange(0,2)
 
-    if number == 0:
-        choice = "NO"
-    else:
-        choice ="YES" 
-    return render(request, 'result.html', {'choice':choice})
+        if number == 0:
+            choice = False
+        else:
+            choice = True 
 
+        new_decision = Decision()
+        new_decision.choice_content = content
+        new_decision.maker = request.user
+        new_decision.result = choice
+        new_decision.save()
+        
+    return render(request, 'decision/result.html', {'decision' : new_decision})
+
+
+def delete(request, decision_id):
+    delete_decision = get_object_or_404(Decision, pk=decision_id)
+    delete_decision.delete()
+    
+    return redirect('decision:list')
