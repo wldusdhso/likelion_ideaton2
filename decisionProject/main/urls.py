6958216feae911eb7b3bd2a4d8b84fcbdcc5d775
dain Hong <ribbon0522@naver.com>
2020-06-03 17:50:30 +0900

    question&main

--- decisionProject/main/urls.py
+++ decisionProject/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/',views.register, name="register"),
+    path('login/',views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
 ]
--- decisionProject/main/views.py
+++ decisionProject/main/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.contrib import auth
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def register(request):
     if request.method=='POST':
-        
+        if request.POST['password']==request.POST['password2']:
+            new_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            
+            auth.login(request, new_user)
+
+            return redirect('main:home')
     else:
-        return render(request, 'register.html')  
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+        auth.login(request, user)
+
+        return redirect('/')
+    else:
+        return render(request,'login.html') 
+
+def logout(request):
+    auth.logout(request)
+    print('logout 실행')
+    return redirect('main:home')
 
     
