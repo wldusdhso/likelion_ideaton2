f31afc1e10f92707c6a1d1f4c2927b6d10d01e0d
JeongJiyeon <wldusdhso@gmail.com>
2020-06-03 16:31:58 +0900

    app name

--- decisionProject/main/views.py
+++ decisionProject/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from vote.models import Question
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    hotQuestions = Question.objects.order_by('-total_votes')[:5]
+    latestQuestions = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'home.html',{
+        'hotQuestions' : hotQuestions,
+        'latestQuestions' : latestQuestions,
+    })
